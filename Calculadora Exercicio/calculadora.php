c93a47281dfa579f1b90bb2f57aa87fa35e618fd
Jeanzinho <jeanaugustopr@gmail.com>
2024-05-07 00:00:31 -0300

    final

--- Calculadora Exercicio/calculadora.php
+++ Calculadora Exercicio/calculadora.php
@@ -4,12 +4,14 @@
 <title>Exemplo</title>
 <meta charset="UTF-8">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<link rel="stylesheet" href="./estilos/estilo.css">
+<link rel="stylesheet" href="./estilos/reset.css">
 </head>
 <body class="p-3 mb-2 bg-secondary text-white">
     <?php
     require "comandos.php";
     
-        echo "<h1>Calculadora PHP</h1>";
+        echo"<h1>Calculadora PHP</h1>";
         formsCalculo();
 
         
